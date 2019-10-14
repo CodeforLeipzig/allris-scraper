2fba41133bd403c0784363881f924ee9fb1c5a33
Lars Henrik Mai <lars.mai@kontinui.de>
2019-10-14 14:43:59 +0200

    Enable HTTP caching

--- allris/settings.py
+++ allris/settings.py
@@ -83,8 +83,10 @@ ROBOTSTXT_OBEY = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 0
+HTTPCACHE_DIR = 'httpcache'
+HTTPCACHE_IGNORE_HTTP_CODES = []
+HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+# Using default 'dummy' policy, which never checks for staleness; not suited for production
+HTTPCACHE_POLICY = 'scrapy.extensions.httpcache.DummyPolicy'
