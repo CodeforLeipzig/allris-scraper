9e0641f678a0b99a8aaa4dcfe95e8f4f4b9b7975
Lars Henrik Mai <lars.mai@kontinui.de>
2019-10-15 09:49:52 +0200

    Restrict meetings scraper to Ratsversammlung for now

--- allris/spiders/meetings.py
+++ allris/spiders/meetings.py
@@ -6,7 +6,8 @@ import json
 class MeetingsSpider(scrapy.Spider):
     name = 'meetings'
     allowed_domains = ['ratsinfo.leipzig.de']
-    start_urls = ['https://ratsinfo.leipzig.de/bi/oparl/1.0/meetings.asp?body=2387']
+    # only scraping "Ratsversammlung" for now
+    start_urls = ['https://ratsinfo.leipzig.de/bi/oparl/1.0/meetings.asp?organization=2387']
 
     def parse(self, response):
         meetings = json.loads(response.text)['data']
