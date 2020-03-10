19dd2c240e935d439b177344d8b01cd997af0537
Lars Henrik Mai <lars.mai@kontinui.de>
2020-03-10 07:24:23 +0100

    Download papers and save the feed to a timestamped file

--- leipzig.py
+++ leipzig.py
@@ -2,28 +2,22 @@ import scrapy
 import scrapy.signals
 from scrapy.crawler import CrawlerProcess
 from allris.spiders.oparl import OparlSpider
-
-def spider_closed(spider, reason):
-    pass
+from pathlib import Path
 
 settings = {
     'HTTPCACHE_ENABLED': True,
     'LOG_LEVEL': 'INFO',
     'FEED_FORMAT': 'jsonlines',
-    'FEED_URI': 'stdout:'
+    'FEED_URI': Path('.') / 'data' / '%(object_type)s_%(time)s.jl'
 }
-process = CrawlerProcess(settings)
 
 spargs = {
     'body_url': 'https://ratsinfo.leipzig.de/bi/oparl/1.0/bodies.asp?id=2387',
     'allowed_domains': ['ratsinfo.leipzig.de'],
-    'object_type': 'meeting',
+    'object_type': 'paper',
     'since': '2020-04-01T00:00:00'
 }
+
+process = CrawlerProcess(settings)
 process.crawl(OparlSpider, **spargs)
-
-for p in process.crawlers:
-    p.signals.connect(spider_closed, signal=scrapy.signals.spider_closed)
-
-
 process.start()
