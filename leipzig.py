813780135ec807a84f607c2154ac3c5b0cc4886d
Tim Feldmueller <feldmueller@posteo.de>
2020-07-01 20:16:23 +0200

    added a function to extract the text from the pdfs as txt

--- leipzig.py
+++ leipzig.py
@@ -6,6 +6,8 @@ from pathlib import Path
 import json
 import requests as re
 from pathlib import Path
+from tika import parser
+import os
 
 settings = {
     "HTTPCACHE_ENABLED": True,
@@ -49,7 +51,27 @@ def download_pdfs():
                         f.write(response.content)
 
 
-process = CrawlerProcess(settings)
-process.crawl(OparlSpider, **spargs)
-process.start()
-download_pdfs()
+def extract_text_from_pdfs_recursively():
+    for root, dirs, files in os.walk("data/pdfs"):
+        for file in files:
+            path_to_pdf = os.path.join(root, file)
+            [stem, ext] = os.path.splitext(path_to_pdf)
+            file_name = stem.split("/")[-1]
+            if ext == ".pdf":
+                print("Processing " + path_to_pdf)
+                pdf_contents = parser.from_file(path_to_pdf)
+                Path("data/txts").mkdir(parents=True, exist_ok=True)
+                path_to_txt = "data/txts/" + file_name + ".txt"
+                with open(path_to_txt, "w") as txt_file:
+                    print("Writing contents to " + path_to_txt)
+                    text = pdf_contents["content"]
+                    txt_file.write(
+                        os.linesep.join([s for s in text.splitlines() if s])
+                    )  # removes empty lines; some lines with just a single white space character still remain, but might be useful for later segmentation?
+
+
+# process = CrawlerProcess(settings)
+# process.crawl(OparlSpider, **spargs)
+# process.start()
+# download_pdfs()
+extract_text_from_pdfs_recursively()
